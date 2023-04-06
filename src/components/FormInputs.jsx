eb0683d01a5f66be186bcc343e3fb0458de23b41
Rahul Dey <jaswant@Jaswants-MacBook-Air.local>
2023-04-06 21:32:04 +0530

    form with validation

--- src/components/FormInputs.jsx
+++ src/components/FormInputs.jsx
@@ -1,12 +1,29 @@
-import React from "react";
-import "../styles/formInput.css";
-const FormInputs = (props) => {
+import { useState } from "react";
+import "./formInput.css";
+
+const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
     <div className="formInput">
-      <label></label>
-      <input placeholder={props.placeholder} />
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
   );
 };
 
-export default FormInputs;
+export default FormInput;
